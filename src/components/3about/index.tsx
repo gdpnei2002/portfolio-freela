8bc26c82c8b180fb6c1c944ddb6bf177e2a9c611
gdpnei2002 <joseneilson25@gmail.com>
2024-03-01 20:17:42 -0300

    refazendo texto

--- src/components/3about/index.tsx
+++ src/components/3about/index.tsx
@@ -7,57 +7,37 @@ export function About() {
       <img id="sobre" src={perfil} alt="" />
       <div>
         <h1>Sobre mim</h1>
-        <p className="">
-          Meu nome é José Neilson, graduado no curso Análise e Desenvolvimento
-          de Sistemas na faculdade, estou sempre em busca de oportunidades para
-          aplicar os meus conhecimentos e adquirir experiência no desenvolvimento
-          de aplicações web.
-          <br />
-          <br />
-          Atualmente aplico o meu conhecimento em desenvolvimentos na{" "}
-          <a href="https://thinkam.net/home" target="_blank" rel="noreferrer">
-            {" "}
-            Think A.M.{" "}
-          </a>{" "}
-          desenvolvendo aplicações web, com objetivo de facilitar o control
-          comercial (serviços/vendas) de um determinado estabelecimento. <br />
-          Desenvolvi front-end de CRM's para alguns sistemas de vendas e
-          criações de landing pages.
-          <br />
-          <br />
-          <br />
-          Também atuo na{" "}
-          <a
-            href="https://www.instagram.com/darwin.soft/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            DarwinSoft{" "}
-          </a>{" "}
-          desenvolvendo aplicações web, com objetivo de facilitar o controle
-          comercial (serviços/vendas) de um determinado estabelecimento. <br />
-          Desenvolvi front-end de CRM e um TMS para a{" "}
-          <a
-            href="https://www.jadlog.com.br/jadlog/home"
-            target="_blank"
-            rel="noreferrer"
-          >
-            jadlog
-          </a>{" "}
-          uma grande transportadora nacional.
-          <br />
+        <p>
+          Com uma trajetória sólida e apaixonada pela arte da programação, me
+          apresento como José Neilson, graduado em Análise e Desenvolvimento de
+          Sistemas. Minha jornada profissional é marcada pela dedicação
+          incansável em transformar conceitos em realidade, impulsionando
+          projetos do início ao fim.
+        </p>{" "}
+        <br />
+        <p>
+          Atualmente, tenho o privilégio de contribuir com a Think AM, onde
+          estou trabalhando em um projeto do inicio ao fim. Desenvolvendo
+          aplicações web, com objetivo de facilitar o control comercial
+          (serviços/vendas) de um determinado estabelecimento. Desenvolvi
+          front-end de CRM's para alguns sistemas de vendas e criações de
+          Landing Pages.
         </p>
-        <h1 id="skills">Skills:</h1>
-        <p className="titulo">Front-End:</p>
-        <p className="skills">
-          HTML5, CSS3, JS, REACT, REACT NATIVE, NEXT E ANGULAR
+        <br />
+        <p>
+          Ao longo da minha carreira, tive a honra de colaborar com empresas de
+          renome de forma Freelancer, sendo elas DarwinSoftware -&gt; jadlog,
+          Genesi -&gt; System Saúde e a empresa chamada Cerbrum, referência em
+          minha cidade.
         </p>
-        <p className="titulo">Estilizações:</p>
-        <p className="skills">STYLED COMPONENTS, SASS, CHAKRA UI.</p>
-        <p className="titulo">Back-End:</p>
-        <p className="skills">NODE, DOCKER, API'S, MY SQL, LARAVEL.</p>
-        {/* <a target="_blank"  href="https://drive.google.com/file/d/1631ZQ6xdGRponXct4cKfZ_sgO7ps6iEt/view?usp=sharing" rel="noreferrer"><button className="curriculo">Currículo</button></a> */}
+        <br />
+        <p>
+          {" "}
+          Meu comprometimento com a excelência técnica, aliado à minha paixão
+          pela inovação, me capacita a enfrentar desafios com confiança e
+          determinação. Estou pronto para abraçar novas oportunidades e
+          continuar deixando minha marca no mundo da tecnologia.
+        </p>
       </div>
     </Container>
   );
